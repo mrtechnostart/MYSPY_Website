8f3cf22ff443585ab192ad054024b6bc7844c521
mrtechnostart <rambpandey238@gmail.com>
2023-05-22 01:20:52 +0530

    Finally Centered A Div

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,17 +4,21 @@ import "./CSS/Landing.css";
 
 const Landing = () => {
   return (
-    <div className="bg-light landingBody">
+    <>
+    <div className="bg-light">
       <Navbar />
       <div className="container">
-        <div className="py-4 text-center primaryContainter">
+        <div className="text-center primaryContainter d-flex align-items-center justify-content-center " style={{minHeight:"90vh"}}>
+          <div className="container">
           <h3 className="landing-title">Hello! It's Madhusudhan Pathak</h3>
           <p className="landing-description px-5">
             This Website is not completely active now. Just after a while, I left YouTube to focus on other aspects. But still, I use this as one of my major platforms to exhibit my new creations, from articles to poetry or anything else that I create. Everything by me is linked up here.
           </p>
+          </div>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,88 +1,13 @@
-import React, { useState } from "react";
-import "./CSS/Navbar.css"
+import React from "react";
+import "./CSS/Navbar.css";
 
 const Navbar = () => {
-  const [offcanvasOpen, setOffcanvasOpen] = useState(false);
-
-  const toggleOffcanvas = () => {
-    setOffcanvasOpen((prevState) => !prevState);
-  };
-
-  const handleLinkClick = () => {
-    setOffcanvasOpen(false);
-  };
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-light px-lg-5">
-      <div className="container-fluid d-flex">
-        <a href="/" className="titletag">MYSPY</a>
-        <button
-          className="navbar-toggler ms-auto"
-          type="button"
-          onClick={toggleOffcanvas}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </div>
-
-      <div
-        className={`offcanvas offcanvas-start ${offcanvasOpen ? "show" : ""}`}
-        tabIndex="-1"
-        id="sidebar"
-        aria-labelledby="sidebarLabel"
-      >
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="sidebarLabel">
-            Menu
-          </h5>
-          <button
-            type="button"
-            className="btn-close text-reset"
-            onClick={toggleOffcanvas}
-            aria-label="Close"
-          ></button>
-        </div>
-        <div className="offcanvas-body">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" to="/" onClick={handleLinkClick}>
-                Home
-              </a> 
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" to="/features" onClick={handleLinkClick}>
-                Features
-              </a> 
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" to="/screenshot" onClick={handleLinkClick}>
-                Screenshot
-              </a> 
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" to="/download" onClick={handleLinkClick}>
-                Downloads
-              </a> 
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://github.com/ProjectBlaze/maintainership/">
-                Maintainership
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://github.com/ProjectBlaze/">
-                Source
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://github.com/ProjectBlaze/official_devices/blob/12.1/changelog.md">
-                Changelog
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <>
+      <h3 className="container-fluid bg-dark text-center py-3">
+        <a href="/" className="text-decoration-none text-white">MYSPY</a>
+      </h3>
+    </>
   );
 };
 
